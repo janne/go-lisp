16f81b6f0456a172ee72f00f3cbd6802392f6219
Jan Andersson <jan.andersson@gmail.com>
2013-09-20 15:01:42 +0200

    Add support for values and parentheses

--- parser.go
+++ parser.go
@@ -1,15 +1,48 @@
 package lisp
 
+import "regexp"
+import "errors"
+
 type Parser struct {
 	tokens []string
 }
 
-func NewParser(program string) *Parser {
-	p := &Parser{}
-	p.tokens = Tokenize(program)
-	return p
+func NewParser(tokens []string) *Parser {
+	return &Parser{tokens: tokens}
 }
 
-func (p *Parser) Parse() string {
-	return "foo"
+func (p *Parser) findEnd(start int) (int, error) {
+	depth := 1
+
+	for i := start; i < len(p.tokens); i++ {
+		t := p.tokens[i]
+		if t == "(" {
+			depth++
+		} else if t == ")" {
+			depth--
+		}
+		if depth == 0 {
+			return i, nil
+		}
+	}
+	return 0, errors.New("List was opened but not closed")
+}
+
+func (p *Parser) Parse() (string, error) {
+	for i := 0; i < len(p.tokens); i++ {
+		t := p.tokens[i]
+		if m, _ := regexp.MatchString("^\\d+$", t); m {
+			return t, nil
+		} else if t == "(" {
+			start := i + 1
+			end, err := p.findEnd(start)
+			if err != nil {
+				return "", err
+			}
+			return NewParser(p.tokens[start:end]).Parse()
+		} else if t == ")" {
+			return "", errors.New("List was closed but not opened")
+		}
+	}
+	return "", errors.New("Invalid input")
 }
--- parser_test.go
+++ parser_test.go
@@ -4,13 +4,31 @@ import "testing"
 
 func TestParse(t *testing.T) {
 	var tests = map[string]string{
-		"42": "42",
+		"42":      "42",
+		"(42)":    "42",
+		"((42))":  "42",
+		"(42 13)": "42",
 	}
 
 	for in, out := range tests {
-		x := NewParser(in).Parse()
+		tokens := Tokenize(in)
+		x, _ := NewParser(tokens).Parse()
 		if x != out {
-			t.Errorf("Parse(%v) = %v, want %v", in, x, out)
+			t.Errorf("Parse('%v') = '%v', want '%v'", in, x, out)
+		}
+	}
+}
+
+func TestParseFailures(t *testing.T) {
+	var tests = []string{
+		"(42",
+	}
+
+	for _, in := range tests {
+		tokens := Tokenize(in)
+		x, err := NewParser(tokens).Parse()
+		if err == nil {
+			t.Errorf("Parse('%v') = '%v', want error", in, x)
 		}
 	}
 }
