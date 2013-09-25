5f01e847bc437523d9f7a256d02e8af1f0a6f0e0
Jan Andersson <jan.andersson@gmail.com>
2013-09-25 16:21:38 +0200

    Refactor func Eval in smaller methods

--- lisp/evaler.go
+++ lisp/evaler.go
@@ -33,6 +33,17 @@ func Eval(expr Sexp) (val interface{}, err error) {
 	return
 }
 
+func evalList(expr []interface{}) (args []interface{}, err error) {
+	for _, i := range expr {
+		if value, err := evalValue(i); err != nil {
+			return nil, err
+		} else {
+			args = append(args, value)
+		}
+	}
+	return
+}
+
 func evalValue(input interface{}) (val interface{}, err error) {
 	switch input.(type) {
 	case int: // Int
@@ -51,86 +62,113 @@ func evalValue(input interface{}) (val interface{}, err error) {
 		if len(expr) > 0 {
 			t := expr[0]
 			if _, ok := t.(Sexp); ok {
-				if val, err = evalValue(t); err == nil {
-					val, err = runProc(val, expr[1:])
-				}
-			} else if t == "quote" { // Quote
-				if len(expr) == 2 {
-					val = expr[1]
-				} else {
-					err = fmt.Errorf("Ill-formed special form: %v", expr)
-				}
-			} else if t == "define" { // Define
-				if len(expr) >= 2 && len(expr) <= 3 {
-					if key, ok := expr[1].(string); ok {
-						if len(expr) == 3 {
-							var i interface{}
-							if i, err = evalValue(expr[2]); err == nil {
-								scope.Create(key, i)
-							}
-						} else {
-							scope.Create(key, nil)
-						}
-						return key, err
-					}
-				}
-				err = fmt.Errorf("Ill-formed special form: %v", expr)
-			} else if t == "set!" { // Set!
-				if len(expr) == 3 {
-					key := expr[1].(string)
-					if _, ok := scope.Get(key); ok {
-						val, err = evalValue(expr[2])
-						if err == nil {
-							scope.Set(key, val)
-						}
-					} else {
-						err = fmt.Errorf("Unbound variable: %v", key)
-					}
-				} else {
-					err = fmt.Errorf("Ill-formed special form: %v", expr)
-				}
-			} else if t == "if" { // If
-				if len(expr) < 3 || len(expr) > 4 {
-					err = fmt.Errorf("Ill-formed special form: %v", expr)
-				} else {
-					r, err := evalValue(expr[1])
-					if err == nil {
-						if r != "false" && r != nil && len(expr) > 2 {
-							val, err = evalValue(expr[2])
-						} else if len(expr) == 4 {
-							val, err = evalValue(expr[3])
-						}
-					}
-				}
-			} else if t == "begin" { // Begin
-				val, err = Eval(expr[1:])
+				val, err = procForm(expr)
+			} else if t == "quote" {
+				val, err = quoteForm(expr)
+			} else if t == "if" {
+				val, err = ifForm(expr)
+			} else if t == "set!" {
+				val, err = setForm(expr)
+			} else if t == "define" {
+				val, err = defineForm(expr)
 			} else if t == "lambda" {
-				if len(expr) > 2 {
-					params := expr[1].(Sexp)
-					val = Proc{params, expr[2:], scope.Dup()}
-				} else {
-					err = fmt.Errorf("Ill-formed special form: %v", expr)
+				val, err = lambdaForm(expr)
+			} else if t == "begin" {
+				val, err = beginForm(expr)
+			} else if isBuiltin(t) {
+				var args []interface{}
+				args, err = evalList(expr[1:])
+				if err == nil {
+					val, err = runBuiltin(t.(string), args)
 				}
-			} else if isBuiltin(t) { // Addition
-				args := make([]interface{}, 0)
-				for _, i := range expr[1:] {
-					if value, err := evalValue(i); err != nil {
-						return nil, err
-					} else {
-						args = append(args, value)
-					}
-				}
-				val, err = runBuiltin(t.(string), args)
 			} else {
-				if val, err = evalValue(t); err == nil {
-					val, err = runProc(val, expr[1:])
-				}
+				val, err = procForm(expr)
 			}
 		}
 	default:
 		err = fmt.Errorf("Unknown data type: %v", input)
 	}
 	return
+}
+
+func procForm(expr []interface{}) (val interface{}, err error) {
+	if val, err = evalValue(expr[0]); err == nil {
+		val, err = runProc(val, expr[1:])
+	}
+	return
+}
+
+func beginForm(expr []interface{}) (val interface{}, err error) {
+	return Eval(expr[1:])
+}
+
+func setForm(expr []interface{}) (val interface{}, err error) {
+	if len(expr) == 3 {
+		key := expr[1].(string)
+		if _, ok := scope.Get(key); ok {
+			val, err = evalValue(expr[2])
+			if err == nil {
+				scope.Set(key, val)
+			}
+		} else {
+			err = fmt.Errorf("Unbound variable: %v", key)
+		}
+	} else {
+		err = fmt.Errorf("Ill-formed special form: %v", expr)
+	}
+	return
+}
+
+func ifForm(expr []interface{}) (val interface{}, err error) {
+	if len(expr) < 3 || len(expr) > 4 {
+		err = fmt.Errorf("Ill-formed special form: %v", expr)
+	} else {
+		r, err := evalValue(expr[1])
+		if err == nil {
+			if r != "false" && r != nil && len(expr) > 2 {
+				val, err = evalValue(expr[2])
+			} else if len(expr) == 4 {
+				val, err = evalValue(expr[3])
+			}
+		}
+	}
+	return
+}
+
+func lambdaForm(expr []interface{}) (val interface{}, err error) {
+	if len(expr) > 2 {
+		params := expr[1].(Sexp)
+		val = Proc{params, expr[2:], scope.Dup()}
+	} else {
+		err = fmt.Errorf("Ill-formed special form: %v", expr)
+	}
+	return
+}
+
+func quoteForm(expr []interface{}) (val interface{}, err error) {
+	if len(expr) == 2 {
+		val = expr[1]
+	} else {
+		err = fmt.Errorf("Ill-formed special form: %v", expr)
+	}
+	return
+}
+
+func defineForm(expr []interface{}) (val interface{}, err error) {
+	if len(expr) >= 2 && len(expr) <= 3 {
+		if key, ok := expr[1].(string); ok {
+			if len(expr) == 3 {
+				var i interface{}
+				if i, err = evalValue(expr[2]); err == nil {
+					scope.Create(key, i)
+				}
+			} else {
+				scope.Create(key, nil)
+			}
+			return key, err
+		}
+	}
+	return nil, fmt.Errorf("Ill-formed special form: %v", expr)
 }
 
 func runProc(proc interface{}, vars []interface{}) (val interface{}, err error) {
