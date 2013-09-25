e7138afc2d9790b7909d6926d088bec5b350dfce
Jan Andersson <jan.andersson@gmail.com>
2013-09-25 14:10:33 +0200

    Add support for interpreting code from STDIN and file

--- go-lisp.go
+++ go-lisp.go
@@ -1,13 +1,39 @@
 package main
 
 import (
-	"fmt"
 	"bufio"
-	"os"
+	"flag"
+	"fmt"
 	"github.com/janne/go-lisp/lisp"
+	"io/ioutil"
+	"os"
 )
 
 func main() {
+	var file = flag.String("file", "", "File to interpret")
+	var repl = flag.Bool("i", false, "Interactive mode")
+	flag.Parse()
+
+	if *repl {
+		Repl()
+	} else if *file != "" {
+		if output, err := ioutil.ReadFile(*file); err != nil {
+			fmt.Printf("ERROR: %v\n", err)
+		} else {
+			if _, err := lisp.EvalString(string(output)); err != nil {
+				fmt.Printf("ERROR: %v\n", err)
+			}
+		}
+	} else {
+		if output, err := ioutil.ReadAll(bufio.NewReader(os.Stdin)); err != nil {
+			fmt.Printf("ERROR: %v\n", err)
+		} else {
+			lisp.EvalString(string(output))
+		}
+	}
+}
+
+func Repl() {
 	fmt.Printf("Welcome to the Lisp REPL\n")
 	reader := bufio.NewReader(os.Stdin)
 	for {
