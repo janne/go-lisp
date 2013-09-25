ecee26db6b2be90a9e88947c3c1e78ba544400df
Jan Andersson <jan.andersson@gmail.com>
2013-09-25 12:09:36 +0200

    No need to repeast type

--- lisp/tokenizer_test.go
+++ lisp/tokenizer_test.go
@@ -16,9 +16,9 @@ func equalSlices(a, b []string) bool {
 
 func TestTokenize(t *testing.T) {
 	var tests = map[string][]string{
-		"(define a 42)":            []string{"(", "define", "a", "42", ")"},
-		"\t(quote\n\t\t(a b c))  ": []string{"(", "quote", "(", "a", "b", "c", ")", ")"},
-		"hello ; dude\n\tworld":    []string{"hello", "world"},
+		"(define a 42)":            {"(", "define", "a", "42", ")"},
+		"\t(quote\n\t\t(a b c))  ": {"(", "quote", "(", "a", "b", "c", ")", ")"},
+		"hello ; dude\n\tworld":    {"hello", "world"},
 	}
 
 	for in, out := range tests {
